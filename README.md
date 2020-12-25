4f1f454f292bdc1f8db53709da04f4cba98e7c22
notepraiya <notepraiya@gmail.com>
2020-12-25 13:09:33 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# baby_names
+
+baby_names app
+
+https://codelabs.developers.google.com/codelabs/flutter-firebase#0
+
---
+++ android/app/src/main/java/com/clubnote/baby_names/MainActivity.java
@@ -0,0 +1,6 @@
+package com.clubnote.baby_names;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
